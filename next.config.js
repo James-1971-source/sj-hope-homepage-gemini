be158dee108c82470a931c19b52ea4f6e65666c7
James-1971-source <metayun1971@gmail.com>
2026-02-03 01:01:52 +0900

    Add Next.js image configuration for external domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+      },
+    ],
+    domains: [
+      'images.unsplash.com',
+      'www.notion.so',
+      'notion.so',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
